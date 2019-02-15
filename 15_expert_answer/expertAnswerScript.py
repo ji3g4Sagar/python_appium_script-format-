22073a07db881736c913f20aceb7703de99be2a9
ji3g4wj6 <h8410312@gmail.com>
2019-02-15 20:34:19 +0800

    edit script 15

--- 15_expert_answer/expertAnswerScript.py
+++ 15_expert_answer/expertAnswerScript.py
@@ -28,7 +28,8 @@ class scriptExpertAnswer():
 	def starter(self):
 		#self.clickExpert()
 		#self.clickExpertDietitian()
-		self.clickExpertPharmacist()
+		#self.clickExpertPharmacist()
+		self.clickExpertGym()
 
 	def clickExpertMedic(self):
 		self.hp.goBackToHomePage()
@@ -59,7 +60,6 @@ class scriptExpertAnswer():
 		self.ck.clickByResourceID(self.apkVersionIdName+"/iv_back")
 		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/tv_quick_search")
 		self.driver.keyevent("4")
-
 	def clickExpertDietitian(self):
 		self.hp.goBackToHomePage()
 		print("-----Start test ", sys._getframe().f_code.co_name, "!!!------")
@@ -88,7 +88,6 @@ class scriptExpertAnswer():
 
 		print("-----Test for ", sys._getframe().f_code.co_name, " finish!!!!!!")
 		sleep(5)
-
 	def clickExpertPharmacist(self):
 		self.hp.goBackToHomePage()
 		print("-----Start test ", sys._getframe().f_code.co_name, "!!!------")
@@ -120,6 +119,37 @@ class scriptExpertAnswer():
 		print("-----Test for ", sys._getframe().f_code.co_name, " finish!!!!!!")
 		sleep(5)
 
+	def clickExpertGym(self):
+		self.hp.goBackToHomePage()
+		print("-----Start test ", sys._getframe().f_code.co_name, "!!!------")
+		self.ck.clickFromManyThingsByResourceID(self.apkVersionIdName+"/home_tab_icon", 2)
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/et_search_keyword")
+		self.sp.swipeLeft(yStart= 0.25)
+		self.ck.clickByString("運動專家")
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/tv_quick_search")
+		self.ck.clickByString(" 快速搜尋 ")
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/tv_sex")
+		self.ck.clickByString("中文(繁體)")
+		sleep(1)
+		self.ck.clickByResourceID(self.apkVersionIdName+"/tv_submit")
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/lin_content")#等待搜尋結果
+		self.ck.clickByResourceID(self.apkVersionIdName+"/lav_favorite")
+		sleep(1)
+		self.ck.clickByResourceID(self.apkVersionIdName+"/tv_expert_name")
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/tv_expert_subject")
+		self.ft.findTextInWholePage("性別")
+		self.ft.findTextInWholePage("關鍵字")
+		self.ft.findTextInWholePage("專長")
+		self.ft.findTextInWholePage("語言")
+		self.ft.findTextInWholePage("問答次數")
+		self.ft.findTextInWholePage("經歷")
+		self.ft.findTextInWholePage("專家認證")
+		self.ck.clickByResourceID(self.apkVersionIdName+"/iv_back")
+		self.wf.explicitWaitByResourceID(self.apkVersionIdName+"/tv_quick_search")
+		self.driver.keyevent("4")
+
+		print("-----Test for ", sys._getframe().f_code.co_name, " finish!!!!!!")
+		sleep(5)
 
 
 
