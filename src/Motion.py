f8029e9a5f0a0e71141c12220d8d5e230927c673
sagar <h8410312@gamil.com>
2019-09-16 16:48:38 +0800

    edit cell_config

--- src/Motion.py
+++ src/Motion.py
@@ -509,7 +509,7 @@ class homePage():
 		#target = self.driver.find_elements_by_id(self.apkVersionIdName+ "/home_tab_icon")
 		#sleep(2)
 		#self.ck.tap(target[0])
-		#self.ck.clickFromManyThingsByResourceID(self.apkVersionIdName+"/home_tab_icon", 0)
+		self.ck.clickFromManyThingsByResourceID(self.apkVersionIdName+"/home_tab_icon", 0)
 		self.ck.clickByResourceID(self.apkVersionIdName+"/home_tab_icon")
 		findNotification = False
 		while (findNotification == False):
